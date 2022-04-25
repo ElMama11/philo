8ed7eca65d222b2b182174ce421351d033e4a250
Maxence Verger <mverger@z3r9p1.42lyon.fr>
2022-04-25 19:10:55 +0200

    add gettime

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mverger <mverger@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 14:51:17 by mverger           #+#    #+#             */
-/*   Updated: 2022/04/24 18:43:03 by mverger          ###   ########.fr       */
+/*   Updated: 2022/04/25 18:15:21 by mverger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ void	eat(t_global *global)
 {
 	struct	timeval start;
 	struct	timeval end;
+	int		i;
 	
+	i = 0;
 	gettimeofday(&start, NULL);
-	if (global->fork_available[global->philo])
+	while (i < global->nb_philo)
+	{
+		
+		i++;
+	}
 }
 
-void	routine(t_global *global) //type void en arg 
+void	routine(void *global) //type void en arg 
 {
-	//eat
+	//eat global->philo[i].forkL et forkR
 	
 	//sleep
 
@@ -41,7 +47,7 @@ int	init_thread(t_global *global)
 		ret = pthread_create(&global->philo[i].thread, NULL, (void *)routine, (void *)global);
 		if (ret != 0)
 		{
-			printf("create thread error\n");
+			printf("thread error\n");
 			return (1);
 		}
 		i++;
